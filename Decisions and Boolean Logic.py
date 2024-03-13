8ab665437315729f8574c69fe2c33cf2643b7d8d
Fcolosimo1 <160653775+Fcolosimo1@users.noreply.github.com>
2024-03-13 16:02:07 -0700

    Update Decisions and Boolean Logic.py
    This update I finished the rest of the code for the part 1 - 3 of the module 4 lab

--- Decisions and Boolean Logic.py
+++ Decisions and Boolean Logic.py
@@ -39,3 +39,50 @@ If myNumber == 83 OR yourNumber == 83 Then
 Else
     Display "83 is not our numbers"
 End If
+
+#Module 4 Lab Part 2 – Pseudocode:  Dual Alternative Decisions
+
+# declare local variables
+monthlySales = 0  # monthly sales amount
+storeAmount = 0  # store bonus amount
+empAmount = 0  # employee bonus amount
+salesIncrease = 0  # percent of sales increase
+prompt = "Enter the monthly sales amount: "  # prompt will be a string literal
+
+# include code to get the monthly Sales
+# include code to get the Increase in Sales
+# include code to Calculate the Store Bonus
+# include code to Calculate the Employee Bonus
+# include code to print out all the results
+
+# This code determines the storeAmount bonus
+if monthlySales >= 110000:
+    storeAmount = 6000
+elif monthlySales >= 100000:
+    storeAmount = 5000
+elif monthlySales >= 90000:
+    storeAmount = 4000
+elif monthlySales >= 80000:
+    storeAmount = 3000
+else:
+    storeAmount = 0
+
+# This code gets the percent of increase in sales
+salesIncrease = float(input("Enter the percent increase in sales (in decimal format): "))
+salesIncrease = salesIncrease * 100  # Convert from decimal to percentage
+
+# This code determines the empAmount bonus
+if salesIncrease >= .05:
+    empAmount = 75
+elif salesIncrease >= 4:
+    empAmount = 50
+elif salesIncrease >= 3:
+    empAmount = 40
+else:
+    empAmount = 0
+
+# This code prints the bonus information
+print("The store bonus amount is $", storeAmount)
+print("The employee bonus amount is $", empAmount)
+if storeAmount == 6000 or empAmount == 75:
+    print('Congrats! You have reached the highest bonus amounts possible!')
